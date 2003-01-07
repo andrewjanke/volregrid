cbf1045c6d77b8287f065357c3a6eab8ab2502be
Andrew Janke <a.janke@gmail.com>
2003-01-07 06:42:27 +0000

    * Finished coordinate parsing of input arb_path coord file * Added -outconfig and associated parsing * Started reading of data file with associated conversions

--- arb_path_io.c
+++ arb_path_io.c
@@ -7,123 +7,134 @@
 
 /* global vars for arb_path_regrid */
 /* list 0 is a special list, (the working buffer one) */
-int      num_lists;
+int      num_lists = 0;
 Coord_list *coord_lists;
 
 /* current affine matrix */
 int      aff_M_identity = 1;
-double   aff_M[3][4] = { {1, 0, 0, 0},
-{0, 1, 0, 0},
-{0, 0, 1, 0}
-};
+double   aff_M[3][4] = { {1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0} };
+
+/* current position in space */
+double   c_pos[3];
+
+/* file pointer for data file */
+FILE    *data_fp = NULL;
 
 /* function prototypes */
-
-/* use realloc to set the size of the point data */
 int      set_coord_list_size(Coord_list list, size_t size);
-
-/* lex bits */
 int      yyflex_init(char *fname);
 int      yyflex_end(void);
 int      yylex(void);
 
-/* init the whole schebang */
-int init_arb_path(char *fname)
-{
-   int id;
 
-   /* initialise the parser with the config file */
-   if(!yyflex_init(fname)){
+/* init the whole schebang */
+int init_arb_path(char *coord_fname, char *data_fname)
+{
+   /* initialise the parser with the coordinates config file */
+   if(!yyflex_init(coord_fname)){
       fprintf(stderr, "Failed to init the parser\n");
       return 0;
       }
 
-   /* init the data store - list[0] */
-   num_lists = 0;
-   id = new_coord_list(0, "default");
-   fprintf(stdout, "Returned list id of %d\n", id);
-
+   /* init the primary coord list - list[0] */
+   if(new_coord_list(0, "DEFAULT") != 0){
+      fprintf(stdout, "Something is amiss, the initial list is not 0!\n");
+      exit(EXIT_FAILURE);
+      }
+   
+   /* open the data file */
+   data_fp = fopen(data_fname, "r");
+   if(data_fp == NULL){
+      fprintf(stderr, "Failed to open the data file %s\n", data_fname);
+      return 0;
+      }
+    
    return 1;
    }
 
 /* finalize the parser */
-void end_arb_path(void){
-   
+void end_arb_path(void)
+{
+
    /* tidy up */
    yyflex_end();
    }
 
 /* get some more co-ordinates, returns an empty list when done */
-Coord_list get_some_arb_path_coords(int max_buf_size)
+Coord_list get_some_arb_path_coords(size_t max_buf_size)
 {
-   int i, j, c;
-   double tmp[3];
    Coord_list list = coord_lists[0];
 
-   fprintf(stdout, "Getting %d coords\n", max_buf_size);
-
-   /* get some co-ordinates */
-   c = 0;
-   while(list->n_pts < max_buf_size && c < max_buf_size){
-      if(!yylex()){
-         fprintf(stdout, "Error in parsing, this isn't good!\n");
-         exit(EXIT_FAILURE);
-         }
-      c++;
+   /* first empty out the list */
+   if(!set_coord_list_size(list, 0)){
+      fprintf(stderr, "Error clearing out list[%d] %s\n", list->id, list->name);
       }
-   
-   /* multiply by the current affine matrix */
-   if(!aff_M_identity){
-      fprintf(stdout, "Multiplying by aff matrix\n");
-      
-      for(c=0; c<list->n_pts; c++){
-         for(i = 0; i < 3; i++){
-            tmp[i] = 0;
-            for(j = 0; j < 3; j++){
-               tmp[i] += aff_M[i][j]* list->pts[c].coord[j];
-               }
-            }
-         
-         list->pts[c].coord[0] = tmp[0] + aff_M[0][3];
-         list->pts[c].coord[1] = tmp[1] + aff_M[1][3];
-         list->pts[c].coord[2] = tmp[2] + aff_M[2][3];
+
+   /* then fill it up again */
+   while (list->n_pts < max_buf_size){
+      if(!yylex()){
+
+         /* exit early if we are done */
+         fprintf(stdout, "yylex() only got %d coords\n", list->n_pts);
+         return (list);
          }
-      
       }
 
    return (list);
    }
 
+/* get some arb_path data, returns NULL on fail                */
+int get_some_arb_path_data(double *data_buf, size_t n_pts, size_t vector_size)
+{
+   
+
+   return 1;
+   }
+
 /* return a new Coord_list's id */
 int new_coord_list(size_t size, char *name)
 {
-   int new_id;
-   
+   int      new_id;
+   int      c;
+
    new_id = num_lists;
-   
+
+   /* first check if a list with that name exists already */
+   for(c = 0; c < num_lists; c++){
+      if(strcmp(coord_lists[c]->name, name) == 0){
+         return 0;
+         }
+      }
+
    /* grow the list of list pointers */
+   num_lists++;
    coord_lists = (Coord_list *) realloc(coord_lists, num_lists * sizeof(*coord_lists));
 
    /* allocate space for the struct */
    coord_lists[new_id] = (Coord_list) malloc(sizeof(*coord_lists[new_id]));
 
    /* init the structure */
-   coord_lists[new_id]->id = num_lists;
+   coord_lists[new_id]->id = new_id;
    strcpy(coord_lists[new_id]->name, name);
-   coord_lists[new_id]->n_pts = 0;
    coord_lists[new_id]->pts = NULL;
 
+   coord_lists[new_id]->n_pts = 0;
+   coord_lists[new_id]->alloc_size = 0;
    set_coord_list_size(coord_lists[new_id], size);
 
-   num_lists++;
    return (new_id);
    }
 
+/* use realloc to set the size of the point data */
 int set_coord_list_size(Coord_list list, size_t size)
 {
+   /* only grow a list, never shrink */
+   if(size > list->alloc_size){
+      list->alloc_size = size;
+      list->pts = (Coord *) realloc(list->pts, list->alloc_size * sizeof(*list->pts));
+      }
 
    list->n_pts = size;
-   list->pts = (Coord *) realloc(list->pts, list->n_pts * sizeof(*list->pts));
 
    return 1;
    }
@@ -154,76 +165,92 @@ void set_curr_matrix(double mat[12])
          c++;
          }
       }
-
-   print_curr_matrix();
    }
 
-int add_coord_to_list(int list_id, double x, double y, double z){
+int add_coord_to_list(int list_id, double x, double y, double z)
+{
    Coord_list list = coord_lists[list_id];
-   int c_pt = list->n_pts;
-   
-   set_coord_list_size(list, c_pt+1);
-   
-   list->pts[c_pt].coord[0] = x;
-   list->pts[c_pt].coord[1] = y;
-   list->pts[c_pt].coord[2] = z;
-   
-   return 1;
-   }
+   int      c_pt = list->n_pts;
 
-int add_rcoord_to_list(int list_id, double x, double y, double z){
-   Coord_list list = coord_lists[list_id];
-   int c_pt = list->n_pts;
-   
-   set_coord_list_size(list, c_pt+1);
-   
-   list->pts[c_pt].coord[0] = list->pts[c_pt-1].coord[0] + x;
-   list->pts[c_pt].coord[1] = list->pts[c_pt-1].coord[1] + y;
-   list->pts[c_pt].coord[2] = list->pts[c_pt-1].coord[2] + z;
-   
-   return 1;
-   }
+   set_coord_list_size(list, list->n_pts + 1);
 
-int add_rrcoord_to_list(int list_id, double x, double y, double z, double rep){
-   Coord_list list = coord_lists[list_id];
-   int c_pt = list->n_pts;
-   int c;
-   
-   set_coord_list_size(list, c_pt+rep);
-   
-   for(c=0; c<rep; c++){
-      list->pts[c_pt+c].coord[0] = list->pts[c_pt+c-1].coord[0] + x;
-      list->pts[c_pt+c].coord[1] = list->pts[c_pt+c-1].coord[1] + y;
-      list->pts[c_pt+c].coord[2] = list->pts[c_pt+c-1].coord[2] + z;
+   /* only apply the matrix if we have to             */
+   /* all lists are referenced to the identity matrix */
+   if(list_id > 0 || aff_M_identity){
+      list->pts[c_pt].coord[0] = x;
+      list->pts[c_pt].coord[1] = y;
+      list->pts[c_pt].coord[2] = z;
       }
-   
-   return 1;
-   }
+   else{
+      int      i;
 
-int get_list_id_from_name(char *name){
-   int c;
-   
-   for(c=0; c<num_lists; c++){
-      if(strcmp(coord_lists[c]->name, name) == 0){
-         return coord_lists[c]->id;
+      fprintf(stdout, "Multiplying");
+      for(i = 0; i < 3; i++){
+         list->pts[c_pt].coord[i] =
+            (aff_M[i][0] * x) + (aff_M[i][1] * y) + (aff_M[i][2] * z) + aff_M[i][3];
          }
       }
-   
-   return 0;
+
+   return 1;
+   }
+
+int add_rcoord_to_list(int list_id, double x, double y, double z, double rep)
+{
+   Coord_list list = coord_lists[list_id];
+   int      c_pt = list->n_pts;
+   int      c;
+
+   set_coord_list_size(list, c_pt + rep);
+
+   /* multiply by the current affine matrix */
+   if(list_id > 0 || aff_M_identity){
+      for(c = 0; c < rep; c++){
+         list->pts[c_pt + c].coord[0] = list->pts[c_pt + c - 1].coord[0] + x;
+         list->pts[c_pt + c].coord[1] = list->pts[c_pt + c - 1].coord[1] + y;
+         list->pts[c_pt + c].coord[2] = list->pts[c_pt + c - 1].coord[2] + z;
+         }
+      }
+   else{
+      int      i;
+
+      fprintf(stdout, "Multiplying");
+      for(c = 0; c < rep; c++){
+
+         for(i = 0; i < 3; i++){
+            list->pts[c_pt + c].coord[i] = list->pts[c_pt + c - 1].coord[i] +
+               (aff_M[i][0] * x) + (aff_M[i][1] * y) + (aff_M[i][2] * z) + aff_M[i][3];
+            }
+         }
+      }
+
+   return 1;
    }
 
 
-int call_list(int list_id){
-   int c;
-   Coord_list list = coord_lists[list_id];
-   
-   /* copy the co-ords into the main (0) list */
-   for(c=0; c<list->n_pts; c++){
-      add_coord_to_list(0, list->pts[c].coord[0],
-                           list->pts[c].coord[1],
-                           list->pts[c].coord[2]);
+int call_list(char *name)
+{
+   int      c = 0;
+   Coord_list list = NULL;
+
+   /* get the list by name */
+   while (c < num_lists && list == NULL){
+      if(strcmp(coord_lists[c]->name, name) == 0){
+         list = coord_lists[c];
+         }
+      c++;
       }
-   
+
+   if(list == NULL){
+      fprintf(stderr, "Couldn't find list called '%s'\n", name);
+      return 0;
+      }
+
+   /* copy the co-ords into the main (0) list */
+   for(c = 0; c < list->n_pts; c++){
+      add_coord_to_list(0, list->pts[c].coord[0],
+                        list->pts[c].coord[1], list->pts[c].coord[2]);
+      }
+
    return 1;
    }
 
@@ -243,8 +270,7 @@ void print_curr_matrix(void)
 {
    int      i;
 
-   fprintf(stdout, "Curr Matrix - %sidentity\n",
-           (aff_M_identity) ? "" : "non-");
+   fprintf(stdout, "Curr Matrix - %sidentity\n", (aff_M_identity) ? "" : "non-");
    for(i = 0; i < 3; i++){
       fprintf(stdout, "  %6.2f %6.2f %6.2f %6.2f\n",
               aff_M[i][0], aff_M[i][1], aff_M[i][2], aff_M[i][3]);
